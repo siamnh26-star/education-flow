688c0da0cbc0c587023abdfbd1e7a92d5b5a3062
siamnh26-star <siamnh26@gmail.com>
2026-05-25 12:10:26 +0600

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,16 @@
-import { handlers } from "@/auth"
-export const { GET, POST } = handlers
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+  ],
+  pages: {
+    signIn: '/auth/signin',
+  },
+});
+
+export { handler as GET, handler as POST };
